33859c05c0141489ab42eff6bab65423b4e797b9
waseem abdallah <abdallah.waseem.1992@gmail.com>
2022-03-19 15:34:51 +0200

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -47,10 +47,10 @@ class DatabaseSeeder extends Seeder
             SurveyQuestionSeeder::class,
             SurveyOptionSeeder::class,
             SurveyAnswerSeeder::class,
-            PermissionTableSeeder::class,
             RoleSeeder::class,
-            ModelHasRoleSeeder::class,
+            PermissionTableSeeder::class,
             RolelHasPermissionsSeeder::class,
+            ModelHasRoleSeeder::class,
         ]);
     }
 }
